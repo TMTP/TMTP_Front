4659e93638e720e62abf1331e41d603dad4fb22d
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-03-11 17:15:28 +0900

    footer css 수정

--- src/components/Layout/footer.jsx
+++ src/components/Layout/footer.jsx
@@ -5,22 +5,27 @@ import { Link } from "gatsby"
 const Footer = () => {
   const { data, copyright } = useFooterDetail()
   return (
-    <div className="flex flex-col justify-center items-center w-full h-[100px] bg-black">
-      <ul className="flex flex-row w-[200px] justify-between mb-[10px] text-white">
-        {data.map((data, index) => {
-          return (
-            <Link to={data.url} key={index}>
-              {data.name}
-            </Link>
-          )
-        })}
-      </ul>
-      <span className=" text-red-800">
-        {copyright.map(data => {
-          return <Link to={data.url}>{data.name} ⓒ.All rights reserved.</Link>
-        })}
-      </span>
-    </div>
+    <footer className="bg-gray-800 text-gray-400 py-4 px-4 sm:flex sm:justify-between">
+      <div className="flex justify-center sm:justify-start mb-2 sm:mb-0">
+        <p className="text-center sm:text-left text-white">
+          {copyright.map(data => {
+            return <Link to={data.url}>{data.name}</Link>
+          })}{" "}
+          ⓒ.All rights reserved.
+        </p>
+      </div>
+      <div className="flex justify-center sm:justify-end">
+        <ul className="flex flex-row w-[200px] justify-between mb-[10px] text-white">
+          {data.map((data, index) => {
+            return (
+              <Link to={data.url} key={index}>
+                {data.name}
+              </Link>
+            )
+          })}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
