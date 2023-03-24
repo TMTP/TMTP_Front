6dc6a6d81dba4ccff6b4c3bcb529b2fb836bd03a
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-03-24 17:35:28 +0900

    Feat: shapeFindBox 뼈대 구성

--- src/container/home/shapeFindBox.jsx
+++ src/container/home/shapeFindBox.jsx
@@ -2,7 +2,16 @@ import React, { useState } from "react";
 import { navigate } from "gatsby";
 
 const ShapeFindBox = () => {
-  return null;
+  return (
+    <div className="w-80 h-80 bg-gray-200 flex flex-col">
+      <div className="bg-blue-500 text-white h-10 flex items-center justify-center w-full">
+        모양으로 찾기
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        Content goes here
+      </div>
+    </div>
+  );
 };
 
 export default ShapeFindBox;
