5a945874d7fac8c91ed1180ae677ae5bfd3e9a6a
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-04-12 23:08:42 +0900

    compare.js 수정

--- src/components/shapeFind/shapeFindBox.jsx
+++ src/components/shapeFind/shapeFindBox.jsx
@@ -14,16 +14,18 @@ const ShapeFindBox = () => {
 
   return (
     <div className="flex flex-col items-center h-full my-10">
-      <div className="w-96 h-96 sm:w-64 sm:h-60  bg-gray-200 flex flex-col rounded-3xl">
+      <div className="w-96 h-96 sm:w-64 sm:h-60 bg-gray-200 flex flex-col rounded-3xl">
         <div className="bg-blue-500 text-white h-10 flex items-center justify-center w-full rounded-xl">
           모양 검색
         </div>
         <div className="flex-col flex ">
           <div className="flex justify-center">
             <SearchBar
+              my="my-3"
               width="w-full"
-              my="my-2"
+              autofocus={false}
               placeholder="약에 적힌 문자검색"
+              hidden="hidden"
             />
           </div>
           <div>
--- src/pages/compare/index.js
+++ src/pages/compare/index.js
@@ -1,75 +1,40 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/layout/layout";
-import Image from "next/image";
+import { getServerSideProps } from "../api/api";
 
-const CompareIndexPage = () => {
-  const [selectedItems, setSelectedItems] = useState([]);
-
+const CompareIndexPage = ({ users }) => {
   const router = useRouter();
-  const { id } = router.query;
+  const { id: ids } = router.query;
+  const idsArray = Array.isArray(ids) ? ids : [ids]; // 쿼리 파라미터를 배열로 변환
 
-  useEffect(() => {
-    const fetchSelectedItems = async () => {
-      try {
-        const response = await fetch(`/api/compare?id=${id}`);
-        const data = await response.json();
-        setSelectedItems(data.filter((item) => id.includes(item.login.uuid)));
-      } catch (error) {
-        console.error(error);
+  const selectedUsers = idsArray
+    .map((id) => {
+      const user = users.find((user) => user.login.uuid === id);
+      if (user && user.login) {
+        return user;
       }
-    };
-    if (id) {
-      fetchSelectedItems();
-    }
-  }, [id]);
+      return null;
+    })
+    .filter((user) => user !== null);
 
   return (
     <main>
       <Layout>
-        <div className="p-4">
-          <h1 className="text-2xl font-bold mb-4 text-center my-4">
-            Selected Information
-          </h1>
-          <table className="w-full table-auto my-2">
-            <thead>
-              <tr className="bg-blue-200 sm:text-xs text-center">
-                <th className="px-2 py-2">Picture</th>
-                <th className="px-4 py-2">Product Name</th>
-                <th className="px-4 py-2">Company Name</th>
-                <th className="px-4 py-2">Formulation</th>
-                <th className="sm:hidden px-4 py-2">Separate</th>
-              </tr>
-            </thead>
-            <tbody>
-              {selectedItems.map((item) => (
-                <tr key={item.login.uuid} className="text-center">
-                  <td className="px-2 py-2">
-                    <Image
-                      src={item.picture.large}
-                      alt={`${item.name.first} ${item.name.last}`}
-                      width={50}
-                      height={50}
-                      className="rounded-full"
-                    />
-                  </td>
-                  <td className="px-4 py-2">
-                    {item.name.first} {item.name.last}
-                  </td>
-                  <td className="px-4 py-2">{item.location.country}</td>
-                  <td className="px-4 py-2">{item.email}</td>
-                  <td className="sm:hidden px-4 py-2">
-                    <button className="text-blue-500">Remove</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <h1>Selected Users:</h1>
+        {selectedUsers.map((user) => (
+          <div key={user.login.uuid}>
+            <p>
+              Name: {user.name.first} {user.name.last}
+            </p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+          </div>
+        ))}
       </Layout>
     </main>
   );
 };
 
+export { getServerSideProps };
 export default CompareIndexPage;
