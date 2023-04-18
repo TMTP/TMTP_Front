8791c950b37a161bee12a6b3b0b9fc58f36d7bf2
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-04-18 17:59:50 +0900

    product 반응형CSS 수정

--- src/components/product/productBoxs.jsx
+++ src/components/product/productBoxs.jsx
@@ -9,30 +9,30 @@ export default function ProductBoxs({ medicineData }) {
       </h1>
       <table className="w-full table-auto ">
         <thead>
-          <tr className="bg-blue-200 sm:text-xs text-center">
-            <th className="px-2 py-2">사진</th>
-            <th className="px-4 py-2">제품명</th>
-            <th className="px-4 py-2">회사명</th>
-            <th className="px-4 py-2">제형</th>
-            <th className=" sm:hidden px-4 py-2">구분</th>
+          <tr className="bg-blue-200 sm:text-[8px] text-center ">
+            <th className="px-2 py-2  border-2 border-black">사진</th>
+            <th className="px-4 py-2 border-2 border-black">제품명</th>
+            <th className="px-4 py-2 border-2 border-black">회사명</th>
+            <th className="px-4 py-2 border-2 border-black">제형</th>
+            <th className=" sm:hidden px-4 py-2 border-2 border-black">구분</th>
           </tr>
         </thead>
         <tbody>
           {medicineData.items.map((result) => (
             <tr
               key={result.ITEM_SEQ}
-              className="border-b-2 border-gray-900 text-xs text-center"
+              className="border-2 border-gray-900 text-xs text-center "
             >
-              <td className="py-2 justify-center flex sm:ml-2">
+              <td className=" justify-center  sm:mt-5  border-r-2 border-black">
                 <Image
                   src={result.ITEM_IMAGE}
                   alt={result.ITEM_IMAGE}
                   width={300}
                   height={300}
-                  className="rounded-full h-16 w-16 sm:h-auto sm:w-auto mr-4"
+                  className="rounded-full h-14 w-14 sm:h-auto sm:w-auto lg:m-4 xl:m-4 "
                 />
               </td>
-              <td className="px-3 py-2">
+              <td className="px-3 py-2 border-r-2 border-black">
                 <Link
                   href={{
                     pathname: "/product/[id]",
@@ -40,9 +40,11 @@ export default function ProductBoxs({ medicineData }) {
                   }}
                 >{` ${result.ITEM_NAME}`}</Link>
               </td>
-              <td className=" py-2">{`${result.ENTP_NAME}`}</td>
-              <td className=" py-2">{`${result.CHART}`}</td>
-              <td className=" sm:hidden  py-2">{result.ITEM_SEQ}</td>
+              <td className=" py-2 border-r-2 border-black">{`${result.ENTP_NAME}`}</td>
+              <td className=" py-2 border-r-2 border-black">{`${result.CHART}`}</td>
+              <td className=" sm:hidden  py-2 border-r-2 border-black">
+                {result.ITEM_SEQ}
+              </td>
             </tr>
           ))}
         </tbody>
--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Layout from "@/components/layout/layout";
 import { fetchMedicineData } from "../api/api";
+import Image from "next/image";
 
 const ProductDetailPage = ({ medicineData }) => {
   const router = useRouter();
@@ -12,11 +13,42 @@ const ProductDetailPage = ({ medicineData }) => {
 
   return (
     <Layout>
-      <div className="bg-white p-6 rounded-md shadow-md ">
-        <h1 className="text-3xl text-center font-bold mb-16 sm:text-base sm:mb-5 text-red-300">
-          {selectedProduct.ITEM_NAME}
-          {selectedProduct.ITEM_SEQ}
-        </h1>
+      <div className="bg-white p-6 flex flex-col">
+        <div className="flex justify-center items-start mb-6">
+          <div className="rounded-lg overflow-hidden shadow-md w-1/2 sm:w-96">
+            <Image
+              src={selectedProduct.ITEM_IMAGE}
+              alt={selectedProduct.ITEM_IMAGE}
+              width={800}
+              height={1000}
+              layout="responsive"
+              className="object-cover h-60 sm:h-96 w-full"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row justify-between">
+          <div className="flex flex-col mb-4">
+            <h1 className="text-3xl font-bold text-center mb-2">
+              {selectedProduct.ITEM_NAME}
+            </h1>
+            <p className="text-gray-600">
+              <span className="font-bold">품목 일련번호: </span>
+              {selectedProduct.ITEM_SEQ}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-bold">업체명: </span>
+              {selectedProduct.ENTP_NAME}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-bold">품목 모양: </span>
+              {selectedProduct.DRUG_SHAPE}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-bold">품목 색상: </span>
+              {selectedProduct.COLOR_CLASS1}
+            </p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
